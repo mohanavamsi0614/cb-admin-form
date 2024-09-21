a2fb775068f012d58e5c7c4fbf42634af9b295c1
Mohana <mohanavamsi14@gmail.com>
2024-09-21 15:18:04 +0530

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,124 @@
-import Image from "next/image";
+"use client"
+
+import axios from "axios";
+import { useState } from "react";
+import { Bounce, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [certificateId, setCertificateId] = useState("");
+  const [name, setName] = useState("");
+  const [regNo, setRegNo] = useState("");
+  const [winningDate, setWinningDate] = useState("");
+  const [winningPlace, setWinningPlace] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleSubmit = () => {
+    const formData = {
+      Certificate_id:certificateId,
+    Name:name,
+    reg_no:Number(regNo),
+    Winning_Date:winningDate,
+    Winning_place:winningPlace,
+    Img_url:imageUrl
+    };
+    if(certificateId && name && regNo && winningDate && winningPlace && imageUrl){
+      console.log(formData)
+      axios.post("https://cb-kare-server.onrender.com/cerficate/add-certificate" ,formData).then((res)=>{
+        toast(res.data.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          transition: Bounce,
+          });
+          
+        console.log(res.data)
+      })
+    }
+    else{
+      alert("fill")
+    }
+};
+
+
+  return (
+    <div className="w-full h-screen bg-gray-500 flex justify-center items-center text-black">
+      
+
+      <div className="p-10 bg-white w-96 rounded-xl shadow-2xl flex flex-col justify-center">
+        <h1 className="text-black text-2xl font-bold">Certificate Adding Form</h1>
+        <div>
+          <input
+            value={certificateId}
+            
+            onChange={(e) => setCertificateId(e.target.value)}
+            placeholder="Certificate ID"
+            className="rounded-lg outline-none ring-none focus:ring-2 focus:border-none focus:ring-orange-400 border border-black w-full p-2 h-10 text-pretty mt-3"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div>
+          <input
+            value={name}
+            
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+            className="rounded-lg border outline-none ring-none focus:ring-2 focus:border-none focus:ring-orange-400 border-black w-full p-2 h-10 text-pretty mt-3"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+        </div>
+        <div>
+          <input
+            value={regNo}
+            
+            onChange={(e) => setRegNo(e.target.value)}
+            placeholder="Reg.No"
+            className="rounded-lg w-full border border-black outline-none ring-none focus:ring-2 focus:border-none focus:ring-orange-400 h-10 p-2 text-pretty mt-3"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+        </div>
+        <div>
+          <input
+            type="date"
+            value={winningDate}
+            
+            onChange={(e)=>{setWinningDate(e.target.value)}}  
+            placeholder="Winning Date"
+            className="rounded-lg border border-black outline-none ring-none focus:ring-2 focus:border-none focus:ring-orange-400 w-full p-2 h-10 text-pretty mt-3"
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+        <div>
+          <input
+            value={winningPlace}
+            
+            onChange={(e) => setWinningPlace(e.target.value)}
+            placeholder="Winning Place"
+            className="rounded-lg border border-black outline-none ring-none focus:ring-2 focus:border-none focus:ring-orange-400 p-2 w-full h-10 text-pretty mt-3"
+          />
+        </div>
+        <div>
+          <input
+            value={imageUrl}
+            
+            onChange={(e) => setImageUrl(e.target.value)}
+            placeholder="Image URL"
+            className="rounded-lg border border-black outline-none ring-none focus:ring-2 focus:border-none focus:ring-orange-400 p-2 w-full h-10 text-pretty mt-3"
+          />
+        </div>
+        <div className="w-full flex justify-center mt-4">
+          <button
+            onClick={handleSubmit}
+            className="w-3/4 h-14 rounded-lg bg-orange-500 text-white font-mono"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    <ToastContainer/>
+
     </div>
   );
 }
